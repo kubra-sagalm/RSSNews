210ba1acdafe0c5b201390164fc17d68a3a6e415
kübra sağlam <kubrasaglam@trakya.edu.tr>
2025-05-05 17:05:29 +0300

    Initial Commit

---
+++ RSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSS
+//
+//  Created by kübra sağlam on 5.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
